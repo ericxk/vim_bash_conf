f48c73aeeb7edf92428036f4b8cc17254f06c17c
feitongxiaoke@gmail.com <ericxu@xukes-MBP.lan>
2015-10-04 11:55:22 +0800

    add some brewcask_install.sh

--- brewcask_install.sh
+++ brewcask_install.sh
@@ -8,3 +8,7 @@ for c in `brew cask list`; do ! brew cask info $c | grep -qF "Not installed" || 
 
 brew cleanup
 brew cask cleanup
+
+##Upgrade pip
+pip list --outdated | grep '^[a-z]* (' | cut -d " " -f 1 | xargs pip install -U
+
