5f0023a5c09bb64262166bd2563c433c00a9d015
ericxu <feitongxiaoke@gmail.com>
2019-05-04 20:35:20 +0800

    update

--- brewcask_install.sh
+++ brewcask_install.sh
@@ -7,7 +7,6 @@ brew update && brew upgrade
 for c in `brew cask list`; do ! brew cask info $c | grep -qF "Not installed" || brew cask install $c; done
 
 brew cleanup
-brew cask cleanup
 
 ##Upgrade pip
-pip3 list --outdated | cut -d " " -f 1 | grep -v "-" | grep -v Package | xargs pip3 install -U
+pip3 list --outdated | cut -d " " -f 1 | grep -v "\--" | grep -v Package | xargs pip3 install -U
